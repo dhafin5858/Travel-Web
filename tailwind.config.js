690a9275d9fcb8af5457f0862f6514bc038f6a59
shinzz2 <dhafin5858@gmail.com>
2022-11-28 14:01:36 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      color: {
+        warna1: '#FA7436'
+      }
+    },
+  },
+  plugins: [],
+}
